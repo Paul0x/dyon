ad28c664ca270f07271dcca384ee62d4cbacba31
= <=>
2016-05-26 18:17:01 -0300

    Adicionado alocação de variáveis no arquivo de hotsite

--- system/includes/control/hotsite/hotsite.php
+++ system/includes/control/hotsite/hotsite.php
@@ -20,15 +20,16 @@
 
 class hotsite {
     /* Database Connection */
+
     private $conn;
-    
+
     /* Hotsite Basic Structure */
     private $event_id;
     private $pages;
     private $event;
     private $status;
     private $last_change;
-    
+
     /* Hotsite CSS Structure */
     private $text_color;
     private $text_font;
@@ -36,55 +37,68 @@ class hotsite {
     private $title_font;
     private $background_color;
     private $background_image;
-    
+
     /* Hotsite Pages Structure */
     private $gallery_status;
     private $contact_status;
     private $schedule_status;
     private $faq_status;
     private $blog_status;
-    
+
     /**
      * Constrói o objeto e inicializa a conexão com o banco de dados.
      */
     public function __construct(conn $conn, $event_id = null) {
         $this->conn = $conn;
-        
-        if(!is_null($event_id)) {
+
+        if (!is_null($event_id)) {
             $this->loadHotsite($event_id);
         }
     }
-    
+
     public function loadHotsite($event) {
-        if(is_array($event)) {
-            if(!is_numeric($event['id'])) {
-                throw new Exception("O identificador do evento está em formato inválido.",1);
+        if (is_array($event)) {
+            if (!is_numeric($event['id'])) {
+                throw new Exception("O identificador do evento está em formato inválido.", 1);
             }
         } else {
-            if(!is_numeric($event)) {
-                throw new Exception("O identificador do evento está em formato inválido.",2);
+            if (!is_numeric($event)) {
+                throw new Exception("O identificador do evento está em formato inválido.", 2);
             }
             $eventcontroller = new eventController();
             $event = $eventcontroller->loadEvent($event);
         }
-        
-        $fields = array("id","id_evento","status","info","data_alteracao");
-        $this->conn->prepareselect("hotsite",$fields,"id_evento",$event['id']);
-        if(!$this->conn->executa()) {
-            throw new Exception("Não foi possível executar a ação.",3);
+        $fields = array("id", "id_evento", "status", "info", "data_alteracao");
+        $this->conn->prepareselect("hotsite", $fields, "id_evento", $event['id']);
+        if (!$this->conn->executa()) {
+            throw new Exception("Não foi possível executar a ação.", 3);
         }
-        
-	$hotsite_array = $this->conn->fetch;
+        $hotsite_array = $this->conn->fetch;
 
-	$variables = $this->setHotsiteVariables($hotsite_array);
-       	
-
-        
+        $variables = $this->getHotsiteVariables($hotsite_array);
     }
 
-    private function setHotsiteVariables(Array $info) {
-        
-        echo "zohan é uma bixa";
-    	
+    private function getHotsiteVariables(Array $hotsite_array) {
+        $variable_list = Array("text_color", "text_font", "title_color", "title_font", "background_image", "background_color", "gallery_status", "contact_status", "schedule_status", "faq_status", "blog_status");
+ 
+
+        try {
+            $info = unserialize($hotsite_array['info']);
+        } catch (Exception $ex) {
+            throw new Exception("Não foi possível pegar as informações do site.");
+        }
+
+        if (is_array($info)) {
+            foreach ($info as $index => $variable) {
+                if (in_array($index, $variable_list)) {
+                    $function = "set" . ucfirst($index);
+                    $this->$function($variable);
+                }
+            }
+            return 2;
+        } else {
+            return 1;
+        }
     }
+
 }
