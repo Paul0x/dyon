5cd84a1d8a5a6eda6f62b73f40865a417a2c43d2
Paulo Felipe Possa Parreira <paul.0@live.de>
2016-05-30 11:39:12 -0300

    Adicionado envio do formulário de alteração das configurações do hotsite.

--- system/js/hotsite.js
+++ system/js/hotsite.js
@@ -66,8 +66,8 @@ hotsiteInterface = function() {
                         if (infos[field] !== null) {
                             pickers[field].fromString(infos[field]);
                         } else {
-                            pickers[field].fromString("ffffff");          
-                            
+                            pickers[field].fromString("ffffff");
+
                         }
                     });
 
@@ -81,11 +81,64 @@ hotsiteInterface = function() {
     };
 
     this.saveHotsiteConfig = function() {
+        var self = this;
+        var color_pattern = /^[0-9A-F]{6}$/;
         var new_infos = new Object();
+        self.hotsiteConfigError("clear");
         new_infos.text_color = $("#hotsite-config-form .item[ref=text-color] .value").html();
         new_infos.title_color = $("#hotsite-config-form .item[ref=title-color] .value").html();
         new_infos.background_color = $("#hotsite-config-form .item[ref=background-color] .value").html();
-        
-        alert(new_infos.text_color+" - "+new_infos.background_color+" - "+new_infos.title_color);
+        new_infos.background_repeat = $("#hotsite-config-form .item[ref=background-image] input[name=background-image-repeat]").val();
+        if (!color_pattern.test(new_infos.text_color)) {
+            self.hotsiteConfigError("A cor do texto está em formato inválido.");
+            return;
+        }
+        if (!color_pattern.test(new_infos.title_color)) {
+            self.hotsiteConfigError("A cor do título está em formato inválido.");
+            return;
+        }
+        if (!color_pattern.test(new_infos.background_color)) {
+            self.hotsiteConfigError("A cor do fundo está em formato inválido.");
+            return;
+        }
+
+        if ($("#hotsite-background-image-file").val().length > 0) {
+            new_infos.background_image = new Object();
+            new_infos.background_image.file = document.getElementById("hotsite-background-image-file").files[0];
+            new_infos.background_image.filename = $("#hotsite-background-image-file").val();
+            new_infos.background_image.extension = new_infos.background_image.filename.substr(new_infos.background_image.filename.length - 3, 3).toLowerCase();
+            if (new_infos.background_image.extension !== "jpg" && new_infos.background_image.extension !== "png" && new_infos.background_image.extension !== "gif") {
+                self.hotsiteConfigError("A imagem que você enviou está em formato inválido.");
+                return;
+            }
+        }
+
+
+
+        var form = new FormData();
+        var xhr = new XMLHttpRequest();
+
+        form.append("mode", "submit_hotsite_config");
+        form.append("text_color", new_infos.text_color);
+        form.append("title_color", new_infos.title_color);
+        form.append("background_color", new_infos.background_color);
+        form.append("background_image", new_infos.background_image.file);
+        form.append("background_image_filename", new_infos.background_image.filename);
+        form.append("background_repeat", new_infos.background_repeat);
+        xhr.open('POST', self.root + "/interface/ajax", true);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 2) {
+            }
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                
+            }
+        };
+        xhr.send(form);
+        alert(new_infos.text_color + " - " + new_infos.background_color + " - " + new_infos.title_color);
+    };
+
+    this.hotsiteConfigError = function(message) {
+        $("#hotsite-config-form .error-log").html(message);
+
     };
 };
