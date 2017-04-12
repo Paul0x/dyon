fea0f9dce1bfa51cc21feba143ae926bb8e0c2dc
Paulo Felipe <paul.0@live.de>
2017-04-12 13:32:41 -0300

    Criação do upload da imagem do usuário

--- js/public/public.js
+++ js/public/public.js
@@ -10,32 +10,13 @@
  *           
  *       Paulo Felipe Possa Parrira [ paul (dot) 0 (at) live (dot) de ]
  *  =====================================================================
- *  File: preferences.js
+ *  File: public.js
  *  Type: Javascript Controller
  *  =====================================================================
  */
 
-preferencesInterface = function() {
+publicInterface = function () {
 
     this.root = $("#dir-root").val();
-    this.init = function() {
-        var self = this;
-        self.setupButtons();
-    };
     
-    this.setupButtons = function() {
-        var self = this;
-        self.bindSidebarTabs();
-    };
-    
-    this.bindSidebarTabs = function() {
-        $("#user-preference-wrap .preference-tabs .item").bind("click", function() {
-           var tab = $(this).attr("tab");
-           $("#user-preference-wrap .preference-tabs .item").removeClass("selected");
-           $(this).addClass("selected");
-           $("#user-preference-wrap .preference-form .tab").css("display","none");
-           $("#user-preference-wrap .preference-form .tab[tab="+tab+"]").css("display","block");
-        });
-    };
-
 };
